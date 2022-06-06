692a488136425044a31d1cc824445063f1bd9a79
Wilson Santos <contato@wilsonsantos.net>
2022-06-05 21:07:14 -0300

    Remove Collection from Category Model to avoid a loop

--- CursoMVC/Models/Category.cs
+++ CursoMVC/Models/Category.cs
@@ -9,6 +9,6 @@ namespace CursoMVC.Models
         public int Id { get; set; }
         public string? Description { get; set; }
         
-        public ICollection<Product> Products { get; set; }
+        //public ICollection<Product> Products { get; set; }
     }
 }
